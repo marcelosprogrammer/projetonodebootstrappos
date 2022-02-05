682c4d32f0c7e47dcbd32b627dbdde120122bf8c
marcelosprogrammer <47003382+marcelosprogrammer@users.noreply.github.com>
2022-02-05 01:12:45 -0300

    acrescentado o servidor em node e a pasta public

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req,res) => {
+const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+
+function serverStaticFile(res,path,contentType, responseCode = 200){
+
+	fs.readFile(__dirname + path, (err,data) => {
+		if (err) {
+			res.writeHead(500, {'Content-Type':'text/plain'})
+			return res.end ('500 - Internal Error')
+		}
+		res.writeHead(responseCode, {'Content-Type':contentType})
+		res.end(data)
+	})
+
+}
+
+switch(path) {
+	case '':
+		serverStaticFile(res, '/public/home.html','text/html')
+		break
+}
+})
+
+server.listen(port, ()=> console.log('servidor inicializado em ${port}' + 'pressione CTRL + C para terminar!'))
